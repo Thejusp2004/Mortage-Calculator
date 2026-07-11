51e762fce38fac4ca4bb6e4624607c667d531575
Thejus <thejuskai@gmail.com>
2026-07-12 02:49:39 +0530

    Made slight changes

--- my_pages/Payment_Schedule.py
+++ my_pages/Payment_Schedule.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 def show():
    
-   col1,col2,col3 = st.columns([3,5,1])
+   col1,col2,col3 = st.columns([3,5,2])
    with col2:
     st.title("📊 Payment Schedule")
     st.caption("Detailed monthly repayment breakdown.")
-    
+
     st.write("")
     st.write("")
     
@@ -18,8 +18,16 @@ def show():
       st.warning("Go to Home page first.")
       return
     df=st.session_state["df"]
-  
+   csv = df.to_csv(index=False)
+   with col3:
+     st.download_button(
+        label="⬇️ Export Schedule",
+        data=csv,
+        file_name="payment_schedule.csv",
+        mime="text/csv"
+    )
    st.dataframe(
     df,
     use_container_width=True,
     height=600)
+   csv = df.to_csv(index=False)
